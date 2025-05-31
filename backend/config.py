267183b580c56dc3a855d8648b33ddcb4af356cf
Maxim <starlitskycode@gmail.com>
2025-06-01 00:20:59 +0200

    Refactor config.py to improve database URI configuration and enhance readability

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,14 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+import os
 
-app = Flask(__name__)
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, instance_relative_config=True)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+db_path = os.path.join(app.instance_path, "mydatabase.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
