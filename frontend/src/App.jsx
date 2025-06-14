0149cab9cc2991f5663d7b921f6828f861095d6e
ElPollaco <elpollaco.official@gmail.com>
2025-06-14 12:58:21 +0200

    added second section with photo changing gallery and randomized 3 teas selection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,78 @@
 // import axios from 'axios';
 import './App.css';
 
+let sessionTeas = null;
+let sessionSeed = null;
+
 function App() {
   // comments only temporary
-  // const [teas, setTeas] = useState([]);
-  // const [message, setMessage] = useState('');
+  const [teas, setTeas] = useState([]);
+  const images = ["tea-image1.png", "tea-image2.png", "tea-image3.png", "tea-image4.png"];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [imageOpacity, setImageOpacity] = useState(1);
+  // fetching teas from backend
+  useEffect(() => {
+    if(sessionTeas && sessionTeas.length > 0){
+      setTeas(sessionTeas);
+    }
+    else{
+      let seed = Date.now();
+      sessionSeed = seed;
+      fetchTeas(sessionSeed);
+    }
+  }, []);
 
-  // useEffect(() => {
-  //   fetchTeas()
-  //   axios.get('http://localhost:5000/')
-  //         .then(response => setMessage(response.data))
-  //         .catch(error => console.error(error));
-  // }, []);
+  // creating interval for changing photo gallery in main site
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setImageOpacity(0);
+      
+      setTimeout(() => {
+        setCurrentImageIndex(prevIndex => 
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+        
+        setImageOpacity(1);
+      }, 350);
+    }, 8000);
 
-  // const fetchTeas = async () => {
-  //   const response = await fetch("http://localhost:5000/teas")
-  //   const data = await response.json()
-  //   setTeas(data.teas)
-  // }
+    return () => clearInterval(interval);
+  }, [images.length]);
 
+  // randomizing 3 teas to be shown at the site
+  const fetchTeas = async (seed) => {
+    try {
+      const response = await fetch("http://localhost:5000/teas");
+      const data = await response.json();
+      
+      let shuffledTeas = shuffle([...data.teas], seed);
+      const selectedTeas = shuffledTeas.slice(0, 3);
+      
+      sessionTeas = selectedTeas;
+      setTeas(selectedTeas);
+      
+    } catch (error) {
+      console.error('Error fetching teas:', error);
+      setTeas([]);
+    }
+  }
+
+  // shuffling teas array for article descriptions
+  function shuffle(array, seed){
+    if (array.length <= 1) return array;
+    
+    let currIndex = array.length;
+    seed = (seed * 9301 + 49297) % 233280;
+
+    while(currIndex !== 0){
+      let randIndex = Math.floor((seed / 233280) * currIndex);
+      currIndex--;
+
+      [array[currIndex], array[randIndex]] = [array[randIndex], array[currIndex]];
+    }
+
+    return array;
+  }
   return (
     <div id="container">
       <header>
@@ -55,16 +109,28 @@ function App() {
                 </div>
             </section>
             <section>
-              <article>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta in quam non lacinia. Donec dolor dolor, fringilla id enim vitae, sagittis cursus velit. Nullam iaculis viverra orci, non dapibus risus bibendum vitae. In et libero in nibh consectetur eleifend eget nec eros. Pellentesque faucibus leo leo, quis cursus purus mollis nec. Cras dapibus porttitor accumsan. Maecenas ultrices, massa vel scelerisque maximus, nisl ex volutpat odio, at mattis neque purus vitae dolor. Proin felis eros, pulvinar quis leo quis, commodo tempor erat. Integer aliquet, diam in mollis posuere, dolor lorem suscipit metus, et accumsan ex lorem id dolor. Nullam porttitor erat nec consequat facilisis. Aliquam iaculis mattis efficitur. Curabitur ut porta augue. Proin leo diam, ultricies in mattis vel, mattis ut arcu.<br></br>
-
-Mauris finibus metus vel commodo sagittis. Donec facilisis ac mi pharetra dignissim. Nam pretium, libero sed malesuada blandit, massa nibh aliquam massa, et pellentesque tortor nisl sed nibh. Phasellus eros risus, congue nec orci id, cursus elementum quam. Nulla in nisl a erat placerat consequat ut quis quam. Pellentesque magna lorem, tincidunt non sollicitudin eu, aliquet non est. Phasellus volutpat libero lorem, non elementum lorem cursus id. Morbi aliquet aliquet pretium. Vivamus at egestas odio, at accumsan nibh. Maecenas quis arcu eu nibh lobortis bibendum. Vivamus luctus ullamcorper elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam nec risus ante.<br></br>
-
-Phasellus ultrices justo ac quam dapibus sollicitudin. Nulla dictum nisl lacus, eget imperdiet quam placerat scelerisque. Vivamus magna massa, mollis non enim a, porttitor elementum nisl. Donec et nibh quam. Nullam porttitor faucibus tellus, a sollicitudin erat aliquam ac. Nam urna felis, lacinia sed nisi sed, condimentum interdum ex. Nulla suscipit suscipit tristique. Cras nulla elit, tincidunt ut sodales a, rhoncus a arcu. Vivamus augue mauris, efficitur quis lacus in, tristique mollis purus. Praesent quis pellentesque neque. Sed vel posuere ante. Nullam fermentum aliquet magna, quis lobortis ex consequat ut. Mauris eu aliquet leo. Integer dignissim quam urna, non malesuada risus sagittis non. Fusce id suscipit quam.<br></br>
-
-Vestibulum accumsan urna libero, ac viverra sem placerat eu. Donec rhoncus ligula nec velit ultricies efficitur. Integer malesuada feugiat vehicula. Sed nec odio eros. Integer luctus, magna at blandit feugiat, sapien urna volutpat leo, et consectetur nisl nulla in justo. Phasellus nisl lectus, dapibus eget velit vel, vestibulum volutpat turpis. Ut pulvinar elit mauris, a venenatis nisl hendrerit quis. Praesent sed scelerisque sem. Sed aliquam eleifend mi, in fermentum ex rutrum a.<br></br>
-
-Praesent sit amet nunc id lorem pellentesque tempor eget quis urna. Sed hendrerit justo non risus consectetur lacinia. Phasellus dignissim massa a volutpat elementum. Integer sed eleifend ante. Quisque ut augue ut risus tempor volutpat. Quisque vitae sodales ligula, in pellentesque velit. Curabitur id pretium risus. Praesent sodales pulvinar lacus ut eleifend. Vestibulum auctor sapien vitae magna pharetra, non porta justo eleifend. Praesent fermentum, urna eu sagittis sagittis, urna dui interdum erat, id ultrices lacus ex in mauris.<br></br>
+              <article id="teas-recommend">
+                <div id="teas-gallery">
+                  <img src={`/images/${images[currentImageIndex]}`} alt="tea" id="tea-image-change" style={{opacity: imageOpacity}}></img>
+                </div>
+                <div id="recommended-teas">
+                  <h1>Our popular products avaiable NOW:</h1>
+                  <div className="tea-product">
+                    {
+                      teas.length > 0 ? (
+                        teas.map((tea) => (
+                          <div key={tea.id} className="tea-item">
+                            <h3>{tea.name}</h3>
+                            <p>{tea.description}</p>
+                          </div>
+                        )
+                      )) : 
+                      (
+                        <p>Currently no teas avaiable. Check out our offer later!</p>
+                      )
+                    }
+                  </div>
+                </div>
               </article>
             </section>
           </div>
